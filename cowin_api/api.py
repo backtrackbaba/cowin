6466b0f25677a8fd8bb68faa7401ec82b23cc72b
Tushar Srivastava <tusharsrivastava162@gmail.com>
2021-05-22 00:59:34 +0530

    minimized filter mechanism

--- cowin_api/api.py
+++ cowin_api/api.py
@@ -2,7 +2,7 @@ from typing import Union, List, Optional
 
 from cowin_api.base_api import BaseApi
 from cowin_api.constants import Constants, Dose, Vaccine
-from cowin_api.utils import today, filter_centers_by_age_limit, filter_centers_by_dose, filter_centers_by_vaccine
+from cowin_api.utils import today, filter_centers
 
 
 class CoWinAPI(BaseApi):
@@ -15,17 +15,14 @@ class CoWinAPI(BaseApi):
         url = f"{Constants.districts_list_url}/{state_id}"
         return self._call_api(url)
 
-    def get_availability_by_base(self,
-                                 caller: str,
+    def get_availability_by_base(self, caller: str,
                                  areas: Union[str, List[str]],
                                  date: str, min_age_limit: int,
                                  vaccine: Optional[Vaccine] = None,
                                  dose: Optional[Dose] = None):
-        """
-        this function is called by the get availability function
+        """this function is called by the get availability function
         this is separated out so that the parent functions have the same
-        structure and development becomes easier
-        """
+        structure and development becomes easier"""
         area_type, base_url = 'pincode', Constants.availability_by_pin_code_url
         if caller == 'district':
             area_type, base_url = 'district_id', Constants.availability_by_district_url
@@ -39,15 +36,12 @@ class CoWinAPI(BaseApi):
         for area_id in areas:
             url = f"{base_url}?{area_type}={area_id}&date={date}"
             curr_result = self._call_api(url)
-
-            if min_age_limit:
-                curr_result = filter_centers_by_age_limit(curr_result, min_age_limit)
-
-            if vaccine:
-                curr_result = filter_centers_by_vaccine(curr_result, vaccine)
-
-            if dose:
-                curr_result = filter_centers_by_dose(curr_result, dose)
+            curr_result = filter_centers(
+                centers=curr_result,
+                min_age_limit=min_age_limit,
+                vaccine=vaccine,
+                dose=dose
+            )
 
             # append
             if 'centers' in curr_result:
@@ -56,28 +50,20 @@ class CoWinAPI(BaseApi):
         # return the results in the same format as returned by the api
         return results
 
-    def get_availability_by_district(self,
-                                     district_id: Union[str, List[str]],
+    def get_availability_by_district(self, district_id: Union[str, List[str]],
                                      date: str = today(),
                                      min_age_limit: int = None,
                                      vaccine: Optional[Vaccine] = None,
                                      dose: Optional[Dose] = None):
-        return self.get_availability_by_base(caller='district',
-                                             areas=district_id,
-                                             date=date,
-                                             min_age_limit=min_age_limit,
-                                             vaccine=vaccine,
-                                             dose=dose)
+        return self.get_availability_by_base(caller='district', areas=district_id,
+                                             date=date, min_age_limit=min_age_limit,
+                                             vaccine=vaccine, dose=dose)
 
-    def get_availability_by_pincode(self,
-                                    pin_code: Union[str, List[str]],
+    def get_availability_by_pincode(self, pin_code: Union[str, List[str]],
                                     date: str = today(),
                                     min_age_limit: int = None,
                                     vaccine: Optional[Vaccine] = None,
                                     dose: Optional[Dose] = None):
-        return self.get_availability_by_base(caller='pincode',
-                                             areas=pin_code,
-                                             date=date,
-                                             min_age_limit=min_age_limit,
-                                             vaccine=vaccine,
-                                             dose=dose)
+        return self.get_availability_by_base(caller='pincode', areas=pin_code,
+                                             date=date, min_age_limit=min_age_limit,
+                                             vaccine=vaccine, dose=dose)
--- cowin_api/utils.py
+++ cowin_api/utils.py
@@ -7,44 +7,24 @@ def today() -> str:
     return datetime.now().strftime(Constants.DD_MM_YYYY)
 
 
-def filter_centers_by_age_limit(centers: dict, min_age_limit: int):
+def filter_centers(centers: dict, min_age_limit: int = None, vaccine: Vaccine = None, dose: Dose = None):
     original_centers = centers.get('centers')
     filtered_centers = {'centers': []}
     for index, center in enumerate(original_centers):
         filtered_sessions = []
         for session in center.get('sessions'):
-            if session.get('min_age_limit') == min_age_limit:
-                filtered_sessions.append(session)
+
+            if min_age_limit and not session.get('min_age_limit') == min_age_limit:
+                continue
+            if vaccine and not session.get('vaccine') == vaccine.value:
+                continue
+            if dose and not session.get(dose.value) > 0:
+                continue
+
+            filtered_sessions.append(session)
+
         if len(filtered_sessions) > 0:
             center['sessions'] = filtered_sessions
             filtered_centers['centers'].append(center)
 
-    return filtered_centers
-
-
-def filter_centers_by_vaccine(centers: dict, vaccine: Vaccine):
-    original_centers = centers.get('centers')
-    filtered_centers = {'centers': []}
-    for index, center in enumerate(original_centers):
-        filtered_sessions = []
-        for session in center.get('sessions'):
-            if session.get('vaccine') == vaccine.value:
-                filtered_sessions.append(session)
-        if len(filtered_sessions) > 0:
-            center['sessions'] = filtered_sessions
-            filtered_centers['centers'].append(center)
-    return filtered_centers
-
-
-def filter_centers_by_dose(centers: dict, dose: Dose):
-    original_centers = centers.get('centers')
-    filtered_centers = {'centers': []}
-    for index, center in enumerate(original_centers):
-        filtered_sessions = []
-        for session in center.get('sessions'):
-            if session.get(dose.value) > 0:
-                filtered_sessions.append(session)
-        if len(filtered_sessions) > 0:
-            center['sessions'] = filtered_sessions
-            filtered_centers['centers'].append(center)
     return filtered_centers
